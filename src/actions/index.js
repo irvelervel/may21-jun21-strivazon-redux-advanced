49ab87841565161824454954a612b2b638e9ac03
Stefano Casasola <ste.casasola@gmail.com>
2021-09-16 10:52:18 +0200

    Injects redux-thunk into the flow

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,3 +12,25 @@ export const setUsernameAction = (name) => ({
   type: 'SET_USERNAME',
   payload: name,
 })
+
+// redux-thunk will allow you to create much more powerful action-creators
+// an action-creator so far is just a function returning an action (a JS object)
+
+// redux-thunk is useful for performing asynchronous operations in redux
+// or very complex synchronous ones
+
+export const addToCartActionThunk = (bookToAdd) => {
+  return async (dispatch, getState) => {
+    // so now I have a function to work with!
+    // this is the perfect place for doing even async operations!
+    // you can delay the dispatching of your action, i.e. waiting for a fetch
+    console.log('In a second I will add the item to the cart!')
+    console.log('a little more...')
+    dispatch({
+      type: 'ADD_ITEM_TO_CART',
+      payload: bookToAdd,
+    })
+  }
+}
+
+// without thunk, redux will not allow you to do this! it will crash :(
